9aa0fa8901815049c970b8a538726babd5684f5b
Fernando Oleo Blanco <irvise@protonmail.com>
2023-01-06 23:57:40 +0100

    Add initial code for a blink

--- src/led_blink.adb
+++ src/led_blink.adb
@@ -1,12 +1,25 @@
-with NeoRV32;
+with NeoRV32; use NeoRV32;
 with Led_Exit;
 
 procedure Led_Blink is
+   Max_Tick : constant := 10_000;
+   type Time_Counter is range 0 .. Max_Tick;
+   Value : Mod_Byte := 0;
+
+   procedure Sleep is
+      Count : Time_Counter := 0;
+   begin
+      while Count < Time_Counter'Last loop
+         Count := @ + 1;
+      end loop;
+   end Sleep;
 
 begin
 
    loop
-      null;
+      Sleep;
+      Value := @ + 1;
+      Set_Leds (Value);
    end loop;
 
    --  Something very bad happened
